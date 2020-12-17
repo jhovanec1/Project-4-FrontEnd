ce597dc46da5bb289f189bf0d6d87191cb64cb75
jhovanec1 <hovanecjoseph@johndeere.com>
2020-12-17 14:38:28 -0600

    undo

--- src/MyOrders.js
+++ src/MyOrders.js
@@ -24,11 +24,7 @@ render(){
         }else{
             hour = hour
         }
-        if(favoriteBook == undefined){
-            favoriteBook.name = ''
-        }else{
-            favoriteBook = favoriteBook
-        }
+        
         let minute = parseFloat(order.createdAt.substring(19,14),10)
         let seconds = parseFloat(order.createdAt.substring(19,17))
         let ampm = ''
